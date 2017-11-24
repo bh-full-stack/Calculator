6dcf8cbd9f7ed7b0762124783badeec774c65853
Mob Machine <mob@stylersonline.com>
2017-11-24 16:09:31 +0100

    Reset display

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 window.onload = function() {
     var display = document.querySelector("#display");
     display.value = "0";
-    document.onkeypress = function(event) {
-        var allowed = event.charCode >= 48 && event.charCode <= 57;
-        console.log(allowed && display.value.length);
+    document.onkeydown = function(event) {
+        // Handle numbers
+        var allowed = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"].indexOf(event.key) > -1;
         if (allowed && (display.value.length < 8)) {
             if (display.value == "0") {
                 display.value = event.key;
             } else {
                 display.value += event.key;
             }
+        }
+        // Clear display
+        if (["Delete", "Backspace"].indexOf(event.key) > -1) {
+            display.value = "0";
         }
     };
 };
