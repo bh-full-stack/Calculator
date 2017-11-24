7b1878be14861947bc14f5b397ecd51d63de1284
Mob Machine <mob@stylersonline.com>
2017-11-24 15:06:21 +0100

    Implement display functionality

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 window.onload = function() {
-    document.querySelector("input").onkeypress = function(event) {
+    var display = document.querySelector("#display");
+    display.value = "0";
+    document.onkeypress = function(event) {
         var allowed = event.charCode >= 48 && event.charCode <= 57;
-        console.log(allowed);
-        if (!allowed) {
-            event.preventDefault();
+        console.log(allowed && display.value.length);
+        if (allowed && (display.value.length < 8)) {
+            if (display.value == "0") {
+                display.value = event.key;
+            } else {
+                display.value += event.key;
+            }
         }
-    }
+    };
 };
